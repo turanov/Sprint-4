37bd93888790501ca618487aad02a0b19d9ed70d
Исламбек Туранов <islambek198@gmail.com>
2021-09-14 16:39:20 +0300

    "Exception" fixed Validator

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,8 @@
 class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
-    // ...
+    INVALID_CHARACTER(100, "Недопустимый символ"),
+    TOO_LONG(101, "Слишком длинный"),
+    NOT_BE_EMPTY(102, "Значение не может быть null"),
+    INVALID_SNILS(103, "Неверный номер СНИЛС")
 }
--- exception/src/main/kotlin/Validator.kt
+++ exception/src/main/kotlin/Validator.kt
@@ -1,23 +1,33 @@
+import kotlin.math.exp
+
 abstract class Validator<T> {
     abstract fun validate(value: T?): List<ErrorCode>
 }
 
 class NameValidator : Validator<String>() {
     override fun validate(value: String?): List<ErrorCode> {
-        val pattern = "[A-ZА-Я][a-zа-я]+".toRegex()
+        val pattern = "[А-Я][а-я]+".toRegex()
 
-        if (value == null || !value.matches(pattern))
-            return listOf(ErrorCode.INVALID_CHARACTER)
+        value?.let {
+            if (!it.matches(pattern))
+                return listOf(ErrorCode.INVALID_CHARACTER)
+            if (it.length > 16)
+                return listOf(ErrorCode.TOO_LONG)
+        } ?: return listOf(ErrorCode.NOT_BE_EMPTY)
         return emptyList()
     }
 }
 
 class PhoneValidator : Validator<String>() {
     override fun validate(value: String?): List<ErrorCode> {
-        val pattern = "(\\+7|8)[0-9]{10}$".toRegex()
+        val pattern = "(7|8)[0-9]{10}$".toRegex()
 
-        if (value == null || !value.matches(pattern))
-            return listOf(ErrorCode.INVALID_CHARACTER)
+        value?.let {
+            if (!it.matches(pattern))
+                return listOf(ErrorCode.INVALID_CHARACTER)
+            if (it.length > 11)
+                return listOf(ErrorCode.TOO_LONG)
+        } ?: return listOf(ErrorCode.NOT_BE_EMPTY)
         return emptyList()
     }
 }
@@ -26,8 +36,12 @@ class EmailValidator : Validator<String>() {
     override fun validate(value: String?): List<ErrorCode> {
         val pattern = "[\\w_\\.]+@\\w+(\\.[A-Za-z]{2,3})$".toRegex()
 
-        if (value == null || !value.matches(pattern))
-            return listOf(ErrorCode.INVALID_CHARACTER)
+        value?.let {
+            if (!it.matches(pattern))
+                return listOf(ErrorCode.INVALID_CHARACTER)
+            if (it.length > 32)
+                return listOf(ErrorCode.TOO_LONG)
+        } ?: return listOf(ErrorCode.NOT_BE_EMPTY)
         return emptyList()
     }
 }
@@ -36,8 +50,28 @@ class SnilsValidator : Validator<String>() {
     override fun validate(value: String?): List<ErrorCode> {
         val pattern = "\\d{11}".toRegex()
 
-        if (value == null || !value.matches(pattern))
-            return listOf(ErrorCode.INVALID_CHARACTER)
+        value?.let {
+            if (!it.matches(pattern)) {
+                println("VALUE = $value")
+                return listOf(ErrorCode.INVALID_CHARACTER)
+            }
+            if (it.length > 11)
+                return listOf(ErrorCode.TOO_LONG)
+            var sum = 0
+            for (i in 0..it.lastIndex - 2) {
+                sum += (it[i] - '0') * (9 - i)
+            }
+            val controlNumber = it.substring(9, 11)
+            var expectedControlNumber = "00"
+            if (sum < 100)
+                expectedControlNumber = if (sum.toString().length < 2) "0${sum}" else sum.toString()
+            else if (sum > 101) {
+                val mod = sum % 101
+                expectedControlNumber = if (mod.toString().length < 2) "0${mod}" else mod.toString()
+            }
+            if (expectedControlNumber != controlNumber)
+                return listOf(ErrorCode.INVALID_SNILS)
+        } ?: return listOf(ErrorCode.NOT_BE_EMPTY)
         return emptyList()
     }
 }
